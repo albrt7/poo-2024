8a81bbf4055e234b03d2bf6da852f72839928bdf
albrt7 <alberto.cirimpei@student.upt.ro>
2024-12-22 15:43:49 +0200

    completarea clasei

--- ProiectPoo/ProiectPoo/Contestatie.cs
+++ ProiectPoo/ProiectPoo/Contestatie.cs
@@ -1,6 +1,14 @@
 namespace ProiectPoo;
-
 public class Contestatie
 {
-    
+    public string Disciplina { get; set; }
+    public string Status { get; set; } // In asteptare, Aprobat, Respins
+    public string Rezultat { get; set; }
+
+    public Contestatie(string disciplina)
+    {
+        Disciplina = disciplina;
+        Status = "In asteptare";
+        Rezultat = "";
+    }
 }
