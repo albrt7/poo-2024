280458040d973be5d8e49153e9f5290ff5742d50
albrt7 <alberto.cirimpei@student.upt.ro>
2024-12-22 15:47:45 +0200

    prima incercare

--- ProiectPoo/ProiectPoo/Program.cs
+++ ProiectPoo/ProiectPoo/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace ProiectPoo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var catalog = new Catalog();
+
+        var student = new Student("Ion Popescu", 2);
+        var disciplina = new Disciplina("Matematica", "Obligatorie");
+        disciplina.Note.Add(new Nota("Activitate", 9));
+        disciplina.Note.Add(new Nota("Examen", 8));
+
+        student.InroleazaDisciplina(disciplina);
+        catalog.AdaugaStudent(student);
+
+        catalog.PublicaNote("Ion Popescu", "Matematica");
+        Console.WriteLine($"Media anuala: {student.CalculeazaMediaAnuala()}");
+    }
+}
