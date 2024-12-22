61b5e413bf2d12d24050b31d17300672583e528c
albrt7 <alberto.cirimpei@student.upt.ro>
2024-12-22 15:41:36 +0200

    completarea clasei disciplina

--- ProiectPoo/ProiectPoo/Disciplina.cs
+++ ProiectPoo/ProiectPoo/Disciplina.cs
@@ -5,5 +5,18 @@ public class Disciplina
     public string Nume { get; set; }
     public string Tip { get; set; }
     public List<Nota>Note { get; set; }
-    
+
+    public Disciplina(string nume, string tip)
+    {
+        Nume = nume;
+        Tip = tip;
+        Note = new List<Nota>();
+    }
+
+    public double CalculeazaMedia()
+    {
+        if(Note.Count == 0)
+            return 0;
+        return Note.Average(n => n.Valoare);
+    }
 }
