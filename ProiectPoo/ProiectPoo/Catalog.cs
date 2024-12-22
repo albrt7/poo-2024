c4219fead3fcffdb8ed9e6137b4f4be1cc1c2f94
albrt7 <alberto.cirimpei@student.upt.ro>
2024-12-22 15:46:12 +0200

    popularea clasei

--- ProiectPoo/ProiectPoo/Catalog.cs
+++ ProiectPoo/ProiectPoo/Catalog.cs
@@ -3,5 +3,44 @@ namespace ProiectPoo;
 public class Catalog
 {
     private List<Student> studenti;
-    
+
+    public Catalog()
+    {
+        studenti = new List<Student>();
+    }
+
+    public void AdaugaStudent(Student student)
+    {
+        studenti.Add(student);
+    }
+
+    public Student GasesteStudent(string nume)
+    {
+        return studenti.FirstOrDefault(s => s.Nume == nume);
+    }
+
+    public void PublicaNote(string numeStudent, string numeDisciplina)
+    {
+        var student = GasesteStudent(numeStudent);
+        if (student != null)
+        {
+            var disciplina = student.Discipline.FirstOrDefault(d => d.Nume == numeDisciplina);
+            if (disciplina != null)
+            {
+                Console.WriteLine($"Note publicate pentru {numeDisciplina}:\n");
+                foreach (var nota in disciplina.Note)
+                {
+                    Console.WriteLine($"{nota.Tip}: {nota.Valoare}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Disciplina nu a fost gasita.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Studentul nu a fost gasit.");
+        }
+    }
 }
